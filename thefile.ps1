46bda80287af53f210b4f0e21414e48eb28a5a42
DaMemon <73153098+DaMemon@users.noreply.github.com>
2024-08-26 14:57:47 +1000

    Update thefile.ps1

--- thefile.ps1
+++ thefile.ps1
@@ -1,10 +1,11 @@
+param(
+    [string]$newArgument = "Tabaniz"
+)
 $nowTime = $(get-date -f dd-MM-yy_hh-mm-ss)
 $startTime = $(get-date -f dd-MM-yy_dddd_hh-mm-ss)
 Start-Transcript -Path C:\Windows\Temp\sfc_$nowTime.log
 ##Write-Information "Script Started - $($startTime)"
 ##$regPath = "HKLM:\SOFTWARE\FSLogix\profiles"
-$newArgument
-
 try
 {
 ##New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force #| Out-Default -Transcript
